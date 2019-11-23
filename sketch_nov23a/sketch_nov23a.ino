e093a6c810afa4f34a3c36e3b9dc0bab372c6ad7
Maya Bielecki <maya.bielecki@gmail.com>
2019-11-23 17:28:52 -0500

    Three microphones working on Arduino Uno

--- sketch_nov23a/sketch_nov23a.ino
+++ sketch_nov23a/sketch_nov23a.ino
@@ -1,24 +1,56 @@
 void setup() {
   Serial.begin(9600);  
   pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
 }
  
 void loop() {
-  int my_val;
+  float my_val;
+  float my_val1;
+  float my_val2;
   int ms = millis();
-  int sum = 0;
+  float sum = 0;
+  float sum1 = 0;
+  float sum2 = 0;
   for (int i = 0; i <4; i++){
     my_val = analogRead(A0);
     sum = sum + my_val;
+    my_val1 = analogRead(A1);
+    sum1 = sum1 + my_val1;
+    my_val2 = analogRead(A2);
+    sum2 = sum2 + my_val2;
   }
-  int final = sum/4;
-  if (final > 31) {  
+  float redThreshold = 22.27;
+  float blueThreshold = 29.27;
+  float greenThreshold = 32;
+  float final0 = sum/4;
+  float final1 = sum1/4;
+  float final2 = sum2/4;
+  if (final0 > redThreshold) {  
     digitalWrite(8, HIGH);
-    Serial.println("TRIGGERED at time:");
+    Serial.println("RED TRIGGERED at time:");
     Serial.println(ms);
     Serial.println("Loudness:");
-    Serial.println(final); 
+    Serial.println(final0); 
+  
+ }if (final1 > blueThreshold) {  
+   digitalWrite(9, HIGH);
+    Serial.println("BLUE TRIGGERED at time:");
+    Serial.println(ms);
+    Serial.println("Loudness:");
+    Serial.println(final1); 
+  }
+  if (final2 > greenThreshold) {  
+  digitalWrite(10, HIGH);
+    Serial.println("GREEN TRIGGERED at time:");
+    Serial.println(ms);
+    Serial.println("Loudness:");
+    Serial.println(final2); 
+  }if (final0 >redThreshold or final1 > blueThreshold or final2 > greenThreshold){
     delay(500);
   }
   digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+ digitalWrite(10, LOW);
 }
